694f2d12c8a84fd220974a983cb1c67ffc4304a2
IsaErtunga <isa.ertunga@gmail.com>
2023-03-12 16:43:44 +0100

    Initial Commit

---
+++ A-DS/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  A-DS
+//
+//  Created by Isa Ertunga on 2023-03-12.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
